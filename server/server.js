118ec1704123f0594c42f55042b9512e2279bc66
Vladzavr <vlad8zavr@gmail.com>
2019-10-12 23:06:34 +0300

    better registration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,19 +7,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const HOST = '127.0.0.1';
 const PORT = 8800;
 const agentsList = [];
 
 // global.pathToRep = process.argv[2];
 
 function registerAgent(info) {
-    let isExist;
-    if (info.port) {
-        isExist = !!(agentsList.filter(item => item === info.port)[0]);
-        //!isExist && agentsList.push(info.port);
-    }
-    if (info.port && !isExist) {
-        agentsList.push(info.port);
+    if (info.host && info.port) {
+        agentsList.push({ host: info.host, port: info.port });
         return true;
     }
     else return false;
@@ -34,6 +30,6 @@ app.post('/notify_agent', (req, res) => {
     console.log('agentList\n', agentsList);
 });
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, HOST, () => {
     console.log('yo dawgs, now listen to the port 8800 (server) ...');
 });
